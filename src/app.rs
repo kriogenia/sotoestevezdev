4f08585a954d058115d2c17933907c347e83ff22
SotoEstevez <ricardo@sotoestevez.dev>
2025-06-03 19:32:24 +0200

    bootstrap something

---
+++ src/app.rs
@@ -0,0 +1,11 @@
+use leptos::prelude::*;
+use leptos::{IntoView, component, view};
+
+#[component]
+pub fn App() -> impl IntoView {
+    view! {
+        <div id="app">
+            <p>Hello World!</p>
+        </div>
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use leptos::prelude::*;
+use app::App;
+
+mod app;
 
 fn main() {
-    leptos::mount::mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    let _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    leptos::mount::mount_to_body(App);
 }
